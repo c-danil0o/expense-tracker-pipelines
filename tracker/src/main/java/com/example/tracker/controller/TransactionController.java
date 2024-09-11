236dd1604412e13702784933d3626e0c996065f9
Danilo Cvijetic <danilo.cvijetic@vega.local>
2024-09-11 09:06:28 +0200

    feat: added remaining transaction crud endpoints

--- tracker/src/main/java/com/example/tracker/controller/TransactionController.java
+++ tracker/src/main/java/com/example/tracker/controller/TransactionController.java
@@ -1,15 +1,14 @@
 package com.example.tracker.controller;
 
-import com.example.tracker.exceptions.TransactionGroupAlreadyExists;
 import com.example.tracker.model.Transaction;
 import com.example.tracker.model.TransactionGroup;
 import com.example.tracker.service.TransactionServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,6 +27,26 @@ public class TransactionController {
     public ResponseEntity<TransactionGroup> createTransactionGroup(@RequestBody TransactionGroup transactionGroup) {
 
         return ResponseEntity.ok(this.transactionService.createGroup(transactionGroup));
+    }
 
+    @PutMapping(value = "/transaction")
+    public ResponseEntity<Transaction> updateTransaction(@RequestBody Transaction transaction){
+        return ResponseEntity.ok(this.transactionService.update(transaction));
+    }
+
+    @DeleteMapping(value = "/transaction/{id}")
+    public ResponseEntity<Void> deleteTransaction(@PathVariable Long id){
+        this.transactionService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/transaction/{id}")
+    public ResponseEntity<Transaction> getTransaction(@PathVariable Long id){
+        return ResponseEntity.ok(this.transactionService.findById(id));
+    }
+
+    @GetMapping(value = "/transaction/all")
+    public ResponseEntity<List<Transaction>> getAllTransactions(){
+        return ResponseEntity.ok(this.transactionService.findAll());
     }
 }

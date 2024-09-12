267cd151ed64b993d8cce87ae22169204660ddfb
Danilo Cvijetic <danilo.cvijetic@vega.local>
2024-09-12 15:39:47 +0200

    fix: moved sender email address to app.prop

--- tracker/src/main/java/com/example/tracker/service/MailService.java
+++ tracker/src/main/java/com/example/tracker/service/MailService.java
@@ -5,6 +5,7 @@ import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +15,13 @@ public class MailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Value("${email.address.report}")
+    private String senderEmailAddress;
+
     public void sendHtmlEmail(String destinationAddress, String htmlContent, String subject) throws MessagingException{
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 
-        mimeMessage.setFrom(new InternetAddress("danilo.cvijetic10@gmail.com"));
+        mimeMessage.setFrom(new InternetAddress(senderEmailAddress));
         mimeMessage.setRecipients(MimeMessage.RecipientType.TO, destinationAddress);
         mimeMessage.setSubject(subject);
         mimeMessage.setContent(htmlContent, "text/html; charset=utf-8");

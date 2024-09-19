5e432a6df0b13e1cb90ecaa9dcb8dd559a9e6370
Danilo Cvijetic <danilo.cvijetic@vega.local>
2024-09-19 13:13:43 +0200

    feat: added request client info to event metadata

--- consumer/src/main/java/com/expense/consumer/service/ListenerService.java
+++ consumer/src/main/java/com/expense/consumer/service/ListenerService.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 public class ListenerService {
     @KafkaListener(topics = "transaction", groupId = "cons", containerFactory = "kafkaRecordListenerContainerFactory")
     public void listenTransactions(GenericRecord record){
+
         System.out.println("Content " + record.get("content"));
+        System.out.println("Client info " + record.get("client_info"));
         System.out.println("Timestamp " + record.get("timestamp"));
         System.out.println("User " + record.get("user_id"));
         System.out.println("Session " + record.get("session_id"));
@@ -19,6 +21,7 @@ public class ListenerService {
     @KafkaListener(topics = "user", groupId = "cons", containerFactory = "kafkaRecordListenerContainerFactory")
     public void listenUser(GenericRecord record){
         System.out.println("Content " + record.get("content"));
+        System.out.println("Client info " + record.get("client_info"));
         System.out.println("Timestamp " + record.get("timestamp"));
         System.out.println("User " + record.get("user_id"));
         System.out.println("Session " + record.get("session_id"));
@@ -30,6 +33,7 @@ public class ListenerService {
     public void listenReminder(GenericRecord record){
         System.out.println("Content " + record.get("content"));
         System.out.println("Timestamp " + record.get("timestamp"));
+        System.out.println("Client info " + record.get("client_info"));
         System.out.println("User " + record.get("user_id"));
         System.out.println("Session " + record.get("session_id"));
         System.out.println("Type " + record.get("type"));
@@ -40,6 +44,7 @@ public class ListenerService {
     public void listenSavings(GenericRecord record){
         System.out.println("Content " + record.get("content"));
         System.out.println("Timestamp " + record.get("timestamp"));
+        System.out.println("Client info " + record.get("client_info"));
         System.out.println("User " + record.get("user_id"));
         System.out.println("Session " + record.get("session_id"));
         System.out.println("Type " + record.get("type"));
--- tracker/src/main/java/com/example/tracker/service/EventStreamServiceImpl.java
+++ tracker/src/main/java/com/example/tracker/service/EventStreamServiceImpl.java
@@ -1,13 +1,18 @@
 package com.example.tracker.service;
 
+import com.eclipsesource.json.JsonObject;
 import com.example.tracker.model.User;
 import com.example.tracker.service.interfaces.EventStreamService;
+import com.example.tracker.utils.UserClientContext;
 import com.example.tracker.utils.kafka.AvroSchemaGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,6 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
@@ -23,7 +29,7 @@ public class EventStreamServiceImpl implements EventStreamService {
     private KafkaTemplate<String, GenericRecord> kafkaTemplate;
 
 
-    private GenericRecord generateAvroRecord(LocalDateTime timestamp, String type, String topic, String payload){
+    private GenericRecord generateAvroRecord(LocalDateTime timestamp, String type, String topic, String payload) throws JsonProcessingException {
         String messageSchema = avroSchemaGenerator.getSchema();
         Schema schema = new Schema.Parser().parse(messageSchema);
         GenericRecord record = new GenericData.Record(schema);
@@ -36,8 +42,12 @@ public class EventStreamServiceImpl implements EventStreamService {
             user = principal.toString();
         }
         String sessionId = RequestContextHolder.currentRequestAttributes().getSessionId();
+        Map<String, String> clientInfo = UserClientContext.getCurrentContext();
+        ObjectMapper objectMapper = new ObjectMapper();
+
 
         record.put("content", payload);
+        record.put("client_info", objectMapper.writeValueAsString(clientInfo));
         record.put("timestamp", timestamp.toString());
         record.put("type", type);
         record.put("user_id", user);
@@ -48,7 +58,12 @@ public class EventStreamServiceImpl implements EventStreamService {
 
     @Override
     public void sendRecord(LocalDateTime timestamp, String type, String topic, String payload){
-        GenericRecord record = generateAvroRecord(timestamp, type, topic, payload);
+        GenericRecord record = null;
+        try {
+            record = generateAvroRecord(timestamp, type, topic, payload);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         ProducerRecord<String, GenericRecord> kafkaRecord = new ProducerRecord<>(topic, null, record);
         this.kafkaTemplate.send(kafkaRecord);
 

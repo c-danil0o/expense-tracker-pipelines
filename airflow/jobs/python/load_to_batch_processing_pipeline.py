d204bf04145921737e6b0fecdaeaf5fa8859177c
Danilo Cvijetic <danilo.cvijetic@vega.local>
2024-09-30 09:14:58 +0200

    fix: fixed driver dependency

--- airflow/jobs/python/load_to_batch_processing_pipeline.py
+++ airflow/jobs/python/load_to_batch_processing_pipeline.py
@@ -1,12 +1,12 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("Bronze_layer_loader").getOrCreate()
+spark = SparkSession.builder.appName("Bronze_layer_loader").config("spark.jars", "/opt/bitnami/spark/jars/mysql-connector-j-9.0.0.jar").getOrCreate()
 
 mysql_url = "jdbc:mysql://mysqldb:3306/expense-tracker"
 properties = {
     "user": "user",
     "password": "password",
-    "driver": "com.mysql.jdbc.Driver"
+    "driver": "com.mysql.cj.jdbc.Driver"
 }
 
 df = spark.read.jdbc(mysql_url, "user", properties=properties)
